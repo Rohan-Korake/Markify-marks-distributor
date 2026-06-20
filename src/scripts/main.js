e13a10380a3f80827cc4ed90396d335d25d091b2
Rohan Korake <rohannkorake@gmail.com>
2026-06-20 20:12:08 +0530

    feat: add input validation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
-console.log("Ready ?");
+import { handleDistribution } from "./distributor.js";
+import { validateInput } from "./validateInput.js";
+
+document.addEventListener("DOMContentLoaded", async () => {});
+
+const distributeButton = document.getElementById("distributeButton");
+distributeButton.addEventListener("click", async () => {
+  if (validateInput()) {
+    await handleDistribution();
+  }
+});
---
+++ src/scripts/validateInput.js
@@ -0,0 +1,76 @@
+export function validateInput() {
+  const totalExperiment = document.getElementById("totalExperiment").value;
+  const maxMark = document.getElementById("maxMark").value;
+  const avgMark = document.getElementById("avgMark").value;
+  const totalExperimentError = document.getElementById("totalExperimentError");
+  const maxMarkError = document.getElementById("maxMarkError");
+  const avgMarkError = document.getElementById("avgMarkError");
+
+  const isNumber = (val) => !isNaN(val);
+  const isPositive = (val) => Number(val) > 0;
+  const isInteger = (val) => {
+    return /^[0-9]+$/.test(val);
+  };
+
+  // TOTAL EXPERIMENT
+  if (!totalExperiment) {
+    showError(totalExperimentError, "Field required.");
+    return false;
+  } else if (!isNumber(totalExperiment)) {
+    showError(totalExperimentError, "Enter a number.");
+    return false;
+  } else if (!isPositive(totalExperiment)) {
+    showError(totalExperimentError, "Must be positive.");
+    return false;
+  } else if (!isInteger(totalExperiment)) {
+    showError(totalExperimentError, "Integers only.");
+    return false;
+  } else {
+    hideError(totalExperimentError);
+  }
+
+  // MAX MARK
+  if (!maxMark) {
+    showError(maxMarkError, "Field required.");
+    return false;
+  } else if (!isNumber(maxMark)) {
+    showError(maxMarkError, "Enter a number.");
+    return false;
+  } else if (!isPositive(maxMark)) {
+    showError(maxMarkError, "Must be positive.");
+    return false;
+  } else if (!isInteger(maxMark)) {
+    showError(maxMarkError, "Integers only.");
+    return false;
+  } else {
+    hideError(maxMarkError);
+  }
+
+  // AVG MARK
+  if (!avgMark) {
+    showError(avgMarkError, "Field required.");
+    return false;
+  } else if (!isNumber(avgMark)) {
+    showError(avgMarkError, "Enter a number.");
+    return false;
+  } else if (!isPositive(avgMark)) {
+    showError(avgMarkError, "Must be positive.");
+    return false;
+  } else {
+    hideError(avgMarkError);
+  }
+
+  return true;
+}
+
+// show error block
+function showError(element, message) {
+  element.classList.remove("hidden");
+  element.innerText = message;
+}
+
+// hide the error block
+function hideError(element) {
+  element.innerText = "";
+  element.classList.toggle("hidden");
+}
